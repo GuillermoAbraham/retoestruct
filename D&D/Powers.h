cccd88acb33be683c4b5c7014c56a0914ad99e82
GuillermoAbraham <68394747+GuillermoAbraham@users.noreply.github.com>
2024-11-04 20:20:39 -0600

    Update Powers.h

--- D&D/Powers.h
+++ D&D/Powers.h
@@ -4,21 +4,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 #ifndef POWERS_H
 #define POWERS_H
-
-
 
 class Powers {
 public:
     Powers(string name, string description) : name(name), description(description){};
-
-
-
+    string getName();
+    string getDescription();
+    void setName(string name):
+    void setDescription(string name);
+    ~Powers();
 private:
     string name, description;
 };
-
-
-
 #endif //POWERS_H

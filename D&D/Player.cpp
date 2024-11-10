e714883dbe6e707e46bef557abb5209b08a0ffe4
Caro-perezVal <a01665909@tec.mx>
2024-11-10 15:45:40 -0600

    Player.cpp

--- D&D/Player.cpp
+++ D&D/Player.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Player.h"
+
+void Player:: setHp(int hp){
+  this->hp=hp;
+}
+
+int Player::getHp(){
+  return hp;
+}
+
+void PLayer:: setLp(int lp){
+  this->lp=lp;
+}
+
+int Player::getLp(){
+  return lp;
+}
+
+void Player::setRace(string race){
+  this->race=race;
+}
+
+string Player::getRace(){
+  return race;
+}
+
+void Player::setName(string name){
+  this->name=name;
+}
+
+string Player::getName(){
+  return name;
+}

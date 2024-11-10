1a06542387cf04be7ff0f27d5f7d68f1ef7a8768
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2024-11-10 14:01:50 -0600

    Update Monster.h

--- D&D/Monster.h
+++ D&D/Monster.h
@@ -1,44 +1,44 @@
 //
 // Created by stanl on 29/10/2024.
 //
-#include <iostream>
-#include <string>
-using namespace std;
-
-#ifndef MONSTER_H
-#define MONSTER_H
-
-
 
 class Monster {
 public:
-    Monster(): name(nullptr), cr(nullptr), type(nullptr),align(nullptr),size(0),armor_class(0),hp(0){};
-    Monster(string name, string cr, string type, string align, int size, int armor_class, int hp): name(name), cr(cr), type(type),align(align),size(size),armor_class(armor_class),hp(hp){};
+    Monster(): name(""), cr(0), type(""),align(""),size(""),armor_class(0),hp(0){};
+    Monster(string name, int cr, string type, string align, string size, int armor_class, int hp): name(name), cr(cr), type(type),align(align),size(size),armor_class(armor_class),hp(hp){};
 
-    ~Monster();
+    ~Monster(){}
     //métodos seters y geters
     void setName(string name);
     string getName();
-    void setCr(string cr);
-    string getCr();
+    void setCr(int cr);
+    int getCr();
     void setType(string type);
     string getType();
     void setAlign(string align);
     string getAlign();
-    void setSize(int size);
-    int getsize();
+    void setSize(string size);
+    string getsize();
     void setArmor_class(int armor_class);
     int getArmor_class();
     void setHp(int Hp);
     int getHp();
-
+    friend std::ostream& operator<<(std::ostream& os,const Monster& M) {
+        os<<"Name: "<<M.name<<" "<<" Cr: "<<M.cr<<" "<<"Type: "<<M.type<<" "<<"Align: "<<M.align<<" "<<"Size: "<<M.size<<" "<<"Armor class: "<<M.armor_class<<" "<<"Hit points: "<<M.hp<<" ";
+        return os;
+    }
+    bool operator==(const Monster& other) const {
+        return (name==other.name && cr==other.cr && type == other.type && align == other.align && size == other.size && armor_class == other.armor_class && hp == other.hp);
+    }
+    bool operator!=(const Monster& other) const {
+        return (name!=other.name || cr!=other.cr || type!= other.type || align != other.align || size != other.size || armor_class != other.armor_class || hp != other.hp);
+    }
 private:
-    string name,cr,type,align;
-    int size,armor_class,hp;
+    string name,type,align,size;
+    int armor_class,hp,cr;
 
 
 };
-
 
 
 #endif //MONSTER_H

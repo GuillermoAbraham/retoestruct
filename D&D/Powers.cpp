ae35843a20dbb07873f646d5c14931069e691b7c
Caro-perezVal <a01665909@tec.mx>
2024-11-10 15:45:15 -0600

    Powers.cpp

--- D&D/Powers.cpp
+++ D&D/Powers.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Powers.h"
+
+void Powers::setName(string name){
+  this->name=name;
+}
+
+string Powers::getName(){
+  return name;
+}
+
+void Powers::setDescription(string description){
+  this->description=description;
+}
+
+string Powers::getDescription(){
+  return name;
+}

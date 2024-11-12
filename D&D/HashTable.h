8a5115587e19983bffe4162283bc4e77df623917
Caro-perezVal <a01665909@tec.mx>
2024-11-12 09:47:18 -0600

    HashTable.h

--- D&D/HashTable.h
+++ D&D/HashTable.h
@@ -1,1 +1,29 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "Node.h"
+
+
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head;
+public:
+    LinkedList();
+    void insertAtStart(T value);
+    void insertAtFinish(T value);
+    void displayList();
+    int findElement(T value);
+    void deleteElement(T value);
+    void read_record();
+    ~LinkedList() {
+        Node<T>* current=head;
+        while(current!=nullptr) {
+            Node<T>* temp= current;
+            current=current->getNext();
+            delete temp;
+        }
+    }
+};
+
+#endif
 

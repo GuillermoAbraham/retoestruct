1e8e7803b23759d6fb33ddf62079dfcd9f560713
GuillermoAbraham <68394747+GuillermoAbraham@users.noreply.github.com>
2024-11-12 10:49:22 -0600

    Create Inventory.h

---
+++ D&D/Inventory.h
@@ -0,0 +1,25 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Inventory {
+public:
+    Inventory(): name(nullptr), description(nullptr), amount(0){};
+    Inventory(string name, string description, int amount): name(name), description(description), amount(amount){};
+
+    ~Inventory();
+    void setName(string name);
+    string getName();
+    void setDescription(string description);
+    string getDescription(string description);
+    void setAmount(int amount);
+    int getAmount();
+private:
+    string name, description;
+    int amount;
+};
+
+#endif //INVENTORY_H

67a684a96a22055f345ddd52557d9200533d7c4f
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2024-11-12 10:55:05 -0600

    Update HashTable.cpp

--- D&D/HashTable.cpp
+++ D&D/HashTable.cpp
@@ -3,32 +3,42 @@
 #include <iostream>
 using namespace std;
 
-int Hashtable::hash(int key) {
-    return key%15;
+#include "Hashtable.h"
+
+template <typename T>
+int Hashtable<T>::hash(int key) {
+    return key%10;
 }
 
-void Hashtable::insert(int data) {
-    int index= hash(data);  
-    Node* newNode= new Node(data);      //implementar colisión
+template <typename T>
+void Hashtable<T>::insert(int data) {
+    int index= hash(data);
+    Node<T>* newNode= new Node(data);
+    //implementar colisión
     newNode->setNext(table[index]);
     table[index]= newNode;
 }
 
-Node* Hashtable::search(int data) {
-    int index= hash(data);              //implementar colisión
+template <typename T>
+Node<T>* Hashtable<T>::search(int data) {
+    int index= hash(data);
+    //implementar colisión
     return table[index];
 }
 
-void Hashtable::remove(int data) {
-    int index= hash(data);              //implementar colision
+template <typename T>
+void Hashtable<T>::remove(int data) {
+    int index= hash(data);
+    //implementar colision
     table[index]= nullptr;
 }
 
-void Hashtable::showTable() {
+template <typename T>
+void Hashtable<T>::showTable() {
     cout<<"--------Table---------"<<endl;
     for (int i=0; i< capacity; i++) {
         if(table[i]!=nullptr){
-            Node* temp= table[i];
+            Node<T>* temp= table[i];
             while(temp!=nullptr) {
                 cout<<temp->getData()<< " "<<temp<<", ";
                 temp= temp->getNext();

9cc2d78290858f044cb7b6f5e6bccf0e4728b86e
Caro-perezVal <a01665909@tec.mx>
2024-11-12 19:14:14 -0600

    Inventory.cpp

--- D&D/Inventory.cpp
+++ D&D/Inventory.cpp
@@ -1,1 +1,27 @@
 #include "Inventory.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+void Inventory::setName(string name) {
+  this->name=name;
+}
+string Inventory::getName(){
+  return this->name;
+}
+void Inventory::setDescription(string description) {
+  this->description=description;
+}
+string Inventory::getDescription(){
+  return this->description;
+}
+void Inventory::setAmount(int amount){
+  this->amount=amount;
+}
+int Inventory::getAmount(){
+  return this->amount;
+}
+

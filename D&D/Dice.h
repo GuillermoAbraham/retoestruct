0edc3cf00d8487178fc2b870877242974fa5ebad
GuillermoAbraham <68394747+GuillermoAbraham@users.noreply.github.com>
2024-11-15 10:31:00 -0600

    Create Dice.h

---
+++ D&D/Dice.h
@@ -0,0 +1,24 @@
+#ifndef DICE_H
+#define DICE_H
+
+#include <string>
+using namespace std;
+
+
+class Dice {
+public:
+    Dice (): face1(8), face2(10){};
+    Dice(int face1, int face2):face1(face1), face2(face2){};
+
+    ~Dice();
+    void setFace1(int face1);
+    int getFace1();
+    void setFace2(int face20);
+    int getFace2();
+private:
+    int face1, face2;
+};
+
+
+
+#endif //DICE_H

29ab574758f1fb312d6a4dfbab3295e2909e32dc
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2024-11-16 22:17:43 -0600

    Update Dice.cpp

--- D&D/Dice.cpp
+++ D&D/Dice.cpp
@@ -1,12 +1,10 @@
 //
-// Created by User1 on 15/11/2024.
+// Created by stanl on 16/11/2024.
 //
-
 #include "Dice.h"
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <cstdlib>
+
 using namespace std;
 
 void Dice::setFace1(int face1) {
@@ -24,13 +22,14 @@ void Dice::setFace2(int face2) {
 int Dice::getFace2() {
     return this->face2;
 }
-void Dice::getRandomDice1() {
-    srand(time(NULL));
-    int random1=rand()%(8-1+1)+1;
-    cout<<random1<<endl;
+float Dice::getRandomDice1() {
+
+    float random1=rand()%(8-1+1)+1;
+    return random1;
 }
 
-void Dice::getRandomDice2() {
-    int random2=rand()%(10-1+1)+1;
-    cout<<random2<<endl;
+float  Dice::getRandomDice2() {
+
+    float random2=rand()%(10-1+1)+1;
+    return random2;
 }

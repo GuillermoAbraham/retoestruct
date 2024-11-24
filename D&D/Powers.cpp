71c32ce536bad42cf019d10d1b5928ca4fb24402
GuillermoAbraham <68394747+GuillermoAbraham@users.noreply.github.com>
2024-11-24 12:56:43 -0600

    Update Powers.cpp

--- D&D/Powers.cpp
+++ D&D/Powers.cpp
@@ -1,6 +1,3 @@
-//
-// Created by stanl on 29/10/2024.
-//
 
 #include "Powers.h"
 #include "Dice.h"
@@ -111,26 +108,46 @@ void Powers::accion(int n, Monster& m, Player& p, int turno) {
 
             break;
         case 7:
-            //Obtienes un dado extra d(10) que te permite volver a usar un hechizo. EL valor del dado de la primer tirada se convierte en HP. HP = d(10) + posible hp de la siguiente tirada
+            //Obtienes un dado extra d(10) que te permite volver a usar un hechizo.
+            //EL valor del dado de la primer tirada se convierte en HP. HP = d(10) + posible hp de la siguiente tirada
             cout<<"\nTurno: "<<turno<<endl;
 
-             m.display();
+            // Turno Player:
+            int extraDamage = dado.getRandomDice2();
+            m.setLp(m.getLp() - extraDamage);
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
+
+            m.display();
             p.display();
 
             break;
         case 8:
             //Los dioses han decidido ayudarte con un ataque letal que inflige dano verdadero. HP = 300
             cout<<"\nTurno: "<<turno<<endl;
-            
-            
+
+            // Turno Player:
+            m.setLp(m.getLp() - 300);
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
+
             m.display();
             p.display();
 
             break;
         case 9:
-            // La lagrima de un gnomo multiversal ha caido sobre tu hermosa cabellera, otorgandote una cantidad considerable de vida. LP = 500
+            // La lagrima de un gnomo multiversal ha caido sobre tu hermosa cabellera,
+            // otorgandote una cantidad considerable de vida. LP = 500
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            // Turno Player:
+            p.setLp(p.getLp() + 500);
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
+
             m.display();
             p.display();
 
@@ -138,7 +155,13 @@ void Powers::accion(int n, Monster& m, Player& p, int turno) {
         case 10:
             // Provoca que el dano de los siguientes 2 ataques del monstruo se reduzcan a la mitad.
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            // Turno Player:
+            cout << "Los siguientes 2 ataques del monstruo infligen la mitad de daño." << endl;
+
+            // Turno Monster:
+            p.setLp(p.getLp() - (dado.getRandomDice1() / 2));
+
             m.display();
             p.display();
 
@@ -146,15 +169,29 @@ void Powers::accion(int n, Monster& m, Player& p, int turno) {
         case 11:
             // Este hechizo inflige dano instananeo. HP = 50 + d(10)
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            // Turno Player:
+            m.setLp(m.getLp() - (50 + dado.getRandomDice2()));
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
+
             m.display();
             p.display();
 
             break;
         case 12:
-            //Obtienes acceso a la lista de mosntruos, y usas el método deleteNode() para borrar a tu oponente de esta realidad. (instakill)
+            //Obtienes acceso a la lista de mosntruos, y usas el
+            //metodo deleteNode() para borrar a tu oponente de esta realidad. (instakill)
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            //Turno player
+            cout<<"¡¿Pero qué esta pasando?!, el mosnstruo se esta desintegrando"<<endl;
+
+            //metodo delete
+        
+            cout<<"Supongo que esto es una victoria para mi"<<endl;
+
             m.display();
             p.display();
 
@@ -162,7 +199,15 @@ void Powers::accion(int n, Monster& m, Player& p, int turno) {
         case 13:
             //Controlas el alma de la bestia, robando puntos de vida a tu favor. LP = d(10) , HP = d(10)
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            // Turno Player:
+            int robaVida = dado.getRandomDice2();
+            p.setLp(p.getLp() + robaVida);
+            m.setLp(m.getLp() - robaVida);
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
+
             m.display();
             p.display();
 
@@ -170,7 +215,12 @@ void Powers::accion(int n, Monster& m, Player& p, int turno) {
         case 14:
             // Infliges dano proporcional al numero de turno actual por combate. HP = d(10) * #turno
             cout<<"\nTurno: "<<turno<<endl;
-            
+
+            // Turno Player:
+            m.setLp(m.getLp() - (dado.getRandomDice2() * turno));
+
+            // Turno Monster:
+            p.setLp(p.getLp() - dado.getRandomDice1());
             m.display();
             p.display();
 
